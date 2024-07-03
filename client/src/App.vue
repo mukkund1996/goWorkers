e967882799bea25c56e91d276ff826c88fee0f78
Mukkund Sunjii <mukkundsunjii@gmail.com>
2024-07-03 20:54:10 +0200

    added client files

---
+++ client/src/App.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import Server from "./components/Server.vue"
+</script>
+
+<template>
+  <header>
+      <nav class="header-container">
+          <h1><span class="go-header">go</span>WORKERS</h1>
+      </nav>
+  </header>
+  <body>
+    <div class="body-container">
+      <h1>More stuff</h1>
+      <Server :isOnline="true"></Server>
+
+    </div>
+  </body>
+</template>
+
+<style scoped>
+body {
+  font-family: Comfortaa;
+}
+h1 {
+  font-style: bold;
+}
+
+.header-container {
+}
+
+.header-container h1,span {
+  font-family: Comfortaa;
+  text-align: center;
+}
+
+.body-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+
+.go-header {
+  color: greenyellow;
+  font-style: bold;
+}
+
+</style>
---
+++ client/src/components/Server.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+import { defineProps } from 'vue';
+const props = defineProps<{isOnline: boolean}>()
+
+</script>
+<template>
+<div :class="props.isOnline ? 'online': 'offline'">
+    <i class="pi pi-cog worker"></i>
+</div>
+</template>
+<style>
+    .worker {
+        font-size: 2.5rem;
+    }
+
+    .online {
+        border: 1.5rem solid green;
+        background-color: lightgreen;
+    }
+    .online .worker {
+        color: green;
+    }
+
+    .offline {
+        border: 1.5rem solid red;
+        background-color: lightcoral;
+    }
+    .offline .worker {
+        color: red
+    }
+</style>
